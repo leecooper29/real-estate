43a5acab9f3169ac38c401a09cb85c4d119e3079
Leandro Cooper <134981673+leecooper29@users.noreply.github.com>
2024-09-25 14:14:43 -0400

    updated real estate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppRoutingModule } from './app.routes';
 import { RentPageComponent } from './rent-page/rent-page.component';
 import { BuyPageComponent } from './buy-page/buy-page.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LogPageComponent } from './log-page/log-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SellPageComponent,
     RentPageComponent,
-    BuyPageComponent
+    BuyPageComponent,
+    LogPageComponent
   ],
   imports: [
     HttpClientModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SellPageComponent } from './sell-page/sell-page.component';
 import { NgModule } from '@angular/core';
 import { RentPageComponent } from './rent-page/rent-page.component';
 import { BuyPageComponent } from './buy-page/buy-page.component';
+import { LogPageComponent } from './log-page/log-page.component';
 
 
 export const routes: Routes = [
@@ -19,6 +20,11 @@ export const routes: Routes = [
     {
         path:'sell-page',
         component: SellPageComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'log-page',
+        component: LogPageComponent,
         pathMatch: 'full'
     },
     {
